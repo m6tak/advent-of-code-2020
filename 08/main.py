18642f873e3a9a93c369e3c8dca17f0c60b3fac7
Michał Szóstak <mszostakk01@gmail.com>
2020-12-08 23:12:00 +0100

    day 8 part 2

--- 08/main.py
+++ 08/main.py
@@ -1,8 +1,21 @@
+def findAllIndicesOfInstruction(instruction_set, instruction):
+  index = 0
+  indices = []
+  for i in instruction_set:
+    if i['name'] == instruction: indices.append(index)
+    index += 1
+  
+  return indices
+
 def executeProgramOnce(instruction_set):
   acc_value = 0
   index = 0
   executed_indices = []
-  while index not in executed_indices:
+  termination_message = 'Natural termination'
+  while index < len(instruction_set):
+    if index in executed_indices:
+      termination_message = "Infinite loop"
+      break
     executed_indices.append(index)
     instruction = instruction_set[index]
     
@@ -20,16 +33,40 @@ def executeProgramOnce(instruction_set):
     if instruction['name'] == 'jmp':
       index += int(instruction['value'][1:]) * sign
  
-  return acc_value
+  return {
+    'acc_value': acc_value,
+    'termination_message': termination_message 
+  }
+
 
 def partI(input_data):
   return executeProgramOnce(input_data)
+
+def partII(input_data):
+  jmp_indices = findAllIndicesOfInstruction(input_data, 'jmp')
+  nop_indices = findAllIndicesOfInstruction(input_data, 'nop')
+  for index in jmp_indices:
+    input_data[index]['name'] = 'nop'
+    res = executeProgramOnce(input_data)
+    if res['termination_message'] == 'Natural termination':
+      return res
+    input_data[index]['name'] = 'jmp'
+  
+  for index in nop_indices:
+    input_data[index]['name'] = 'jmp'
+    res = executeProgramOnce(input_data)
+    if res['termination_message'] == 'Natural termination':
+      return res
+    input_data[index]['name'] = 'nop'
+  
+  return None
 
 
 def main():
   lines = open('in.txt').readlines()
   instruction_set = [{'name': line.split()[0], 'value': line.split()[1]} for line in lines]
   print(partI(instruction_set))
+  print(partII(instruction_set))
 
 
 if __name__ == "__main__":
